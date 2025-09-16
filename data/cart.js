7417d18e7c5a487360f305f44bfec864abd54e58
Mihir1101-ai <choudharimihir08@gmail.com>
2025-09-16 21:57:45 +0530

    Add files via upload

---
+++ data/cart.js
@@ -0,0 +1,17 @@
+export const cart=[{
+  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity:2
+},{productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',quantity:1}];
+export function addtocart(product){  let matchingitem;
+  cart.forEach((item)=>{
+    if(product===item.productId){
+        matchingitem=item;
+    }
+  });
+
+  if(matchingitem){
+    matchingitem.quantity +=1;
+  }
+  else{cart.push({productId: product,
+    quantity:1
+  });}}
